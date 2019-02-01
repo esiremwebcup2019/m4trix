319e73ede90533d7d377cf8a234dc4af73cecdb9
PrimPom <primouspomalegni2@gmail.com>
2019-02-01 21:18:29 +0100

    BeauGoss

---
+++ Formulaire/insertion.php
@@ -0,0 +1,23 @@
+<?php
+	/*$pdo = null;
+	$dsn ='mysql: host = localhost; dbname= webCup ';
+	$dbUser = 'root';
+	$pw = '';
+
+	try{
+		$pdo = new PDO($dsn,$dbUser, $pw );
+		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+	}
+	catch(PDOException $e){
+		echo 'Erreur de connexion :' $e->getMessage();
+	}
+
+	$pdo->query("SET NAMES UTF8");//Solution pour l'encodage UTF8
+	return $pdo;*/
+
+	var_dump($_POST);
+
+
+
+?>
