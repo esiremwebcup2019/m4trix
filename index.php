30a9e2528954e6a554913f82feb1ce8700e5947e
joel <akougbe@gmail.com>
2019-02-02 09:42:17 +0100

    version finale

--- index.php
+++ index.php
@@ -22,7 +22,38 @@ switch ($request_uri[0]) {
         require __DIR__.'/src/reservation.php';
 
         break;
+
+    case '/about':
+    require __DIR__.'/src/about.php';
+    break;
+
+    case '/anniversaire':
+    require __DIR__.'/src/anniversaire.php';
+    break;
+
+    case '/contact':
+    require __DIR__.'/src/contact.php';
+    break;
+
+    case '/fille':
+    require __DIR__.'/src/fille.php';
+    break;
+
+    case '/garcon':
+    require __DIR__.'/src/garcon.php';
+    break;
+
+    case '/mariage':
+    require __DIR__.'/src/mariage.php';
+    break;
+
+    case '/voyage':
+    require __DIR__.'/src/voyage.php';
+    break;
+
+
     // Everything else
+
     default:
         header('HTTP/1.0 404 Not Found');
       require __DIR__.'/template/404.html';
