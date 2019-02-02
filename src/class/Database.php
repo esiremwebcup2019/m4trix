557072b391396930ffdf6e3e09bc79a283d4ae03
joel <akougbe@gmail.com>
2019-02-02 03:19:50 +0100

    update Henri

--- src/class/Database.php
+++ src/class/Database.php
@@ -14,17 +14,23 @@ $charset = 'utf8mb4';
 
 class Db
 {
+  private $host = 'localhost';
+  private $db   = 'm4trix';
+  private $user = 'root';
+  private $pass = '';
+  private $charset = 'utf8mb4';
+
   protected $base;
 
   public function __construct(){
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false,
     ];
     try {
-         $this->base = new PDO($dsn, $user, $pass, $options);
+         $this->base = new PDO($dsn, $this->user, $this->pass, $options);
     } catch (\PDOException $e) {
          throw new \PDOException($e->getMessage(), (int)$e->getCode());
        }
--- src/home.php
+++ src/home.php
@@ -1,3 +1,11 @@
 <?php
+
+if(isset($_SESSION['status'])){
+  if($_SESSION['status']==1){
+    echo '<script>alert("Réversation enregistrée");</script>';
+    $_SESSION['status']=0;
+  }
+}
+
 require __DIR__.'/../template/index.html';
  ?>
